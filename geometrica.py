744da58478f399c8e119f32111652cbf827b38eb
João Dias <joao.dias@temasistemas.com.br>
2021-02-10 01:03:51 -0300

    Complemento da função de calcular área

--- geometrica.py
+++ geometrica.py
@@ -1,5 +1,5 @@
 
 
 def calcular_area_retangulo(base: int, altura: int, unidade_medida: str):
-    # Exemplo '45 m2'
-    pass
+    area = base * altura
+    return str(area) + unidade_medida
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from geometrica import *
 if __name__ == '__main__':
     hello_world()
     print("-------------Iniciando calculadora----------------")
-    calcular_area_retangulo(4, 5, 'm2')
+    print(calcular_area_retangulo(4, 5, 'm2'))
+    print(calcular_area_retangulo(4, 4, 'm2'))
     print("-------------Finalizando calculadora----------------")
