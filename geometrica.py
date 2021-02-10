f6fd9feb3a5db33bc4a85980ea506f88acda8059
João Dias <joao.dias@temasistemas.com.br>
2021-02-10 00:34:55 -0300

    First commit

---
+++ geometrica.py
@@ -0,0 +1,5 @@
+
+
+def calcular_area_retangulo(base: int, altura: int, unidade_medida: str):
+    # Exemplo '45 m2'
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+from helloworld import hello_world
+from geometrica import *
+
+if __name__ == '__main__':
+    hello_world()
+    print("-------------Iniciando calculadora----------------")
+    calcular_area_retangulo(4, 5, 'm2')
+    print("-------------Finalizando calculadora----------------")
